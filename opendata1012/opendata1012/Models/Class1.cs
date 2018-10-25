865dca813495c8929d275f2914cb456a21d167d5
君豪 林 <xxz3155@gmail.com>
2018-10-25 22:46:46 +0800

    add

---
+++ opendata1012/opendata1012/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opendata1012.Models
+{
+    class Class1
+    {
+        public int id { get; set; }
+        public string 資料集名稱 { get; set; }
+        public string 服務分類 { get; set; }
+
+    }
+}
